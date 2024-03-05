492ee671de3d980df8c952b225783f88b4aa63f7
PlebSauce <119638801+PlebSauce@users.noreply.github.com>
2024-03-05 13:29:07 -0800

    Full basic implementation. Finish print and hash support.

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -2,6 +2,7 @@ package evaluator
 
 import (
 	"Projects/Interpreter/object"
+	"fmt"
 )
 
 var builtins = map[string]*object.Builtin{
@@ -90,6 +91,14 @@ var builtins = map[string]*object.Builtin{
 			newElements[length] = args[1]
 
 			return &object.Array{Elements: newElements}
+		},
+	},
+	"puts": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			for _, arg := range args {
+				fmt.Println(arg.Inspect())
+			}
+			return NULL
 		},
 	},
 }
