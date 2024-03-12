090ac00b409d478bbfb36fabe7352c984f22122e
PlebSauce <119638801+PlebSauce@users.noreply.github.com>
2024-03-12 13:35:08 -0700

    Add basic quote and unquote functionality.

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -18,7 +18,7 @@ var builtins = map[string]*object.Builtin{
 			case *object.String:
 				return &object.Integer{Value: int64(len(arg.Value))}
 			default:
-				return newError("argument to 'len' not supported, got %s", args[0].Type())
+				return newError("argument to `len` not supported, got %s", args[0].Type())
 			}
 		},
 	},
--- object/object.go
+++ object/object.go
@@ -19,6 +19,7 @@ const (
 	BUILTIN_OBJ      = "BUILTIN"
 	ARRAY_OBJ        = "ARRAY"
 	HASH_OBJ         = "HASH"
+	QUOTE_OBJ        = "QUOTE"
 )
 
 type ObjectType string
@@ -177,4 +178,13 @@ func (h *Hash) Inspect() string {
 	out.WriteString("}")
 
 	return out.String()
+}
+
+type Quote struct {
+	Node ast.Node
+}
+
+func (q *Quote) Type() ObjectType { return QUOTE_OBJ }
+func (q *Quote) Inspect() string {
+	return "QUOTE(" + q.Node.String() + ")"
 }
